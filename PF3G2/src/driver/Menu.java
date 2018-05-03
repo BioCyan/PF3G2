18eefc313e73decff641bf4b5ccdb4fabc050482
Kevin Jones <bioncl7@gmail.com>
2018-05-03 08:33:04 -0500

    Moved everything to the left to account for smaller screen sizes

--- PF3G2/src/driver/Menu.java
+++ PF3G2/src/driver/Menu.java
@@ -6,37 +6,37 @@ public class Menu implements driver.GameInterface{
 	public static void startScreen(Graphics graphics) {
 		graphics.setFont(new Font( "SansSerif", Font.BOLD, 30));
 		graphics.setColor(Color.cyan);
-		graphics.drawString("The J.U.M.P. Unusual Multi-dimensional Platformer", (int)screenSize.getWidth()/2, (int)(screenSize.getHeight()/2)); 
+		graphics.drawString("The J.U.M.P. Unusual Multi-dimensional Platformer", 60, (int)(screenSize.getHeight()/2)); 
 		graphics.setFont(new Font( "SansSerif", Font.BOLD, 20 ));
 		graphics.setColor(Color.white);
-		graphics.drawString("Press Enter to Begin!", (int)screenSize.getWidth()/2, (int)(screenSize.getHeight()/2+20));	
-		graphics.drawString("PRESS 'I' FOR INSTRUCTION MENU", (int)screenSize.getWidth()/2, (int)(screenSize.getHeight()/2+60));
+		graphics.drawString("Press Enter to Begin!", 60, (int)(screenSize.getHeight()/2+20));	
+		graphics.drawString("PRESS 'I' FOR INSTRUCTION MENU", 60, (int)(screenSize.getHeight()/2+60));
 	}
 	
 	public static void pauseScreen(Graphics graphics) {
 		graphics.setFont(new Font( "SansSerif", Font.BOLD, 30));
 		graphics.setColor(Color.white);
-		graphics.drawString("GAME PAUSED", (int)screenSize.getWidth()/2, (int)(screenSize.getHeight()/2)); 
+		graphics.drawString("GAME PAUSED", 60, (int)(screenSize.getHeight()/2)); 
 		graphics.setFont(new Font( "SansSerif", Font.BOLD, 20 ));
-		graphics.drawString("PRESS 'P' TO UNPAUSE", (int)screenSize.getWidth()/2, (int)(screenSize.getHeight()/2+20));
+		graphics.drawString("PRESS 'P' TO UNPAUSE", 60, (int)(screenSize.getHeight()/2+20));
 	}
 	public static void instructionScreen(Graphics graphics) {
 		graphics.setFont(new Font( "SansSerif", Font.BOLD, 30));
 		graphics.setColor(Color.BLACK);
 		graphics.fillRect(0, 0, (int)screenSize.getWidth(), (int)screenSize.getHeight());
 		graphics.setColor(Color.white);
-		graphics.drawString("---GAME INSTRUCTIONS---", (int)screenSize.getWidth()/2, (int)(screenSize.getHeight()/2));
+		graphics.drawString("---GAME INSTRUCTIONS---", 60, (int)(screenSize.getHeight()/2));
 		graphics.setFont(new Font( "SansSerif", Font.BOLD, 20));
-		graphics.drawString("Press Enter to Begin!", (int)screenSize.getWidth()/2, (int)(screenSize.getHeight()/2+20));	
-		graphics.drawString("Use W/A/S/D to move around, space to jump, and P to pause.", (int)screenSize.getWidth()/2, (int)(screenSize.getHeight()/2+40));
+		graphics.drawString("Press Enter to Begin!", 60, (int)(screenSize.getHeight()/2+20));	
+		graphics.drawString("Use W/A/S/D to move around, space to jump, and P to pause.", 60, (int)(screenSize.getHeight()/2+40));
 	}
 	public static void endScreen(Graphics graphics, String finalTime) {
 		graphics.setFont(new Font( "SansSerif", Font.BOLD, 50));
 		graphics.setColor(Color.red);
-		graphics.drawString("GAME OVER", (int)screenSize.getWidth()/2, (int)(screenSize.getHeight()/2)); 
-		graphics.drawString("PRESS 'R' TO RESTART", (int)screenSize.getWidth()/2, (int)(screenSize.getHeight()/2+40));
+		graphics.drawString("GAME OVER", 60, (int)(screenSize.getHeight()/2)); 
+		graphics.drawString("PRESS 'R' TO RESTART", 60, (int)(screenSize.getHeight()/2+40));
 		graphics.setColor(Color.white);
-		graphics.drawString(finalTime, (int)screenSize.getWidth()/2, (int)(screenSize.getHeight()/2+80)); 
+		graphics.drawString(finalTime, 60, (int)(screenSize.getHeight()/2+80)); 
 	}
 }
 
