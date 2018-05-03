437158ac66701c6594a7b34247d75f112d727df4
Brian <Briansankey92@yahoo.com>
2018-05-02 20:02:24 -0500

    Added more to level two. Added in air friction to userMovement class, player class, and game class.

--- PF3G2/src/levels/Level2.java
+++ PF3G2/src/levels/Level2.java
@@ -13,10 +13,16 @@ public class Level2 implements LevelInterface {
 		platforms = new ArrayList<Block>();
 		//platforms
 		platforms.add(new Block(new Vector(0, 0.25f, 0), new Vector(2, 0.5f, 2)));
-		platforms.add(new Block(new Vector(6, -0.50f, 6), new Vector(8, -0.25f, 8)));
-		platforms.add(new Block(new Vector(0, -1.25f, 10), new Vector(2, -1, 12)));
+		platforms.add(new Block(new Vector(5, 0.25f, 0), new Vector(7, 0.5f, 2)));
+		platforms.add(new Block(new Vector(10, 0.25f, 0), new Vector(12, 0.5f, 2)));
+		platforms.add(new Block(new Vector(8, 0.25f, 2), new Vector(14, 10, 4)));
+		//after corner
+		platforms.add(new Block(new Vector(14, 0.25f, 3), new Vector(16, 0.5f, 5)));
+		platforms.add(new Block(new Vector(16, 0.25f, 8), new Vector(2, 0.5f, 2)));
+		/*platforms.add(new Block(new Vector(6, -0.50f, 6), new Vector(8, -0.25f, 8)));
+		platforms.add(new Block(new Vector(0, -1.25f, 12), new Vector(2, -1, 14)));
 		//ending
-		platforms.add(getEndBlock());
+*/		platforms.add(getEndBlock());
 	}
 	
 	public List<Block> getBlocks() {
@@ -28,7 +34,7 @@ public class Level2 implements LevelInterface {
 	}
 
 	public Block getEndBlock() {
-		return new Block(new Vector(-1, -1, -1), new Vector(0, -.5f, 0));
+		return new Block(new Vector(-3, -1, -3), new Vector(-1, -.5f, -1));
 	}
 
 	public float resetYValue() {
