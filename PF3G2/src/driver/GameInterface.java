127971c8a47b7efb66b418bba5854945ec606ec5
jonathantan <jonathantan@jonathans-macbook-pro.local>
2018-05-02 23:33:04 -0500

    Change Jump speed to 6

--- PF3G2/src/driver/GameInterface.java
+++ PF3G2/src/driver/GameInterface.java
@@ -4,7 +4,7 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 
 public interface GameInterface {
-	public final float ACCELERATIONOFGRAVITY=15,FRICTION=24,ACCELERATION=60, MAXSPEED=6, JUMPSPEED = 16;
+	public final float ACCELERATIONOFGRAVITY=15,FRICTION=24,ACCELERATION=60, MAXSPEED=6, JUMPSPEED = 6;
 	public float sensitivity = 5.0f/1000;
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 }
