69f4af569acc630c61cc3b505eda8f33e4e714e3
Brian <Briansankey92@yahoo.com>
2018-05-02 19:01:14 -0500

    Finished Level one and have a new starting area for level 2

--- PF3G2/src/levels/Level1.java
+++ PF3G2/src/levels/Level1.java
@@ -7,26 +7,29 @@ import java.util.ArrayList;
 
 public class Level1 implements LevelInterface {
 	private List<Block> platforms;
-	private Level2 level2;
 	
 	public Level1() {
 		platforms = new ArrayList<Block>();
 		//floor
-				platforms.add(new Block(new Vector(-1, -2, -1), new Vector(8, -1, 8)));
-				//walls
-				platforms.add(new Block(new Vector(-1, -1, -1), new Vector(8, 10, 0)));
-				platforms.add(new Block(new Vector(8, -1, 0), new Vector(9, 10, 8)));
-				platforms.add(new Block(new Vector(-1, -1, 8), new Vector(8, 10, 9)));
-				platforms.add(new Block(new Vector(-2, -1, -1), new Vector(-1, 10, 8)));
-				//platforms
-				platforms.add(new Block(new Vector(2, -1, 2), new Vector(4, -0.25f, 4)));
-				platforms.add(new Block(new Vector(4, -0.25f, 4), new Vector(6, 0.50f, 6)));
-				platforms.add(new Block(new Vector(4, 0.5f, 0), new Vector(6, 1, 2)));
-				platforms.add(new Block(new Vector(6, 1.01f, 2), new Vector(8, 1.75f, 4)));
-				platforms.add(new Block(new Vector(6, 1.75f, 6), new Vector(8, 2.50f, 8)));
-				platforms.add(new Block(new Vector(0, 1.75f, 6), new Vector(2, 2.50f, 8)));
-				platforms.add(getEndBlock());
-		level2 = new Level2();
+			platforms.add(new Block(new Vector(-1, -2, -1), new Vector(8, -1, 8)));
+			//walls
+			platforms.add(new Block(new Vector(-1, -1, -1), new Vector(8, 10, 0)));
+			platforms.add(new Block(new Vector(8, -1, 0), new Vector(9, 10, 8)));
+			platforms.add(new Block(new Vector(-1, -1, 8), new Vector(8, 10, 9)));
+			platforms.add(new Block(new Vector(-2, -1, -1), new Vector(-1, 10, 8)));
+			//platforms
+			platforms.add(new Block(new Vector(2, -1, 2), new Vector(4, -0.25f, 4)));
+			platforms.add(new Block(new Vector(4, -0.25f, 4), new Vector(6, 0.50f, 6)));
+			platforms.add(new Block(new Vector(4, 0.5f, 0), new Vector(6, 1, 2)));
+			platforms.add(new Block(new Vector(6, 1.01f, 2), new Vector(8, 1.75f, 4)));
+			platforms.add(new Block(new Vector(6, 1.75f, 6), new Vector(8, 2.50f, 8)));
+			platforms.add(new Block(new Vector(0, 2.25f, 6), new Vector(2, 2.50f, 8)));
+			platforms.add(new Block(new Vector(0, 2.25f, 1), new Vector(2, 2.50f, 3)));
+			//exit blocks
+			platforms.add(new Block(new Vector(2, 2.25f, 0), new Vector(3, 4.50f, 1)));
+			platforms.add(new Block(new Vector(-1, 2.25f, 0), new Vector(0, 4.50f, 1)));
+			platforms.add(new Block(new Vector(-1, 4.50f, 0), new Vector(3, 5.50f, 1)));
+			platforms.add(getEndBlock());
 	}
  
 	public List<Block> getBlocks() {
@@ -34,11 +37,11 @@ public class Level1 implements LevelInterface {
 	}
 
 	public Vector getStartPosition() {
-		return new Vector(1, 2, 1);
+		return new Vector(1, 0, 1);
 	}
 
 	public Block getEndBlock() {
-		Block end = new Block(new Vector(0, -1, 0), new Vector(0.75f, -.5f, 0.75f));
+		Block end = new Block(new Vector(0, 2.25f, 0), new Vector(2, 2.50f, 1));
 		return end;
 	}
 
@@ -53,6 +56,6 @@ public class Level1 implements LevelInterface {
 	}
 
 	public LevelInterface nextLevel() {
-		return level2;
+		return new Level2();
 	}
 }
--- PF3G2/src/levels/Level2.java
+++ PF3G2/src/levels/Level2.java
@@ -8,17 +8,14 @@ import model.Block;
 public class Level2 implements LevelInterface {
 
 	private List<Block> platforms;
-	private Level2 level2;
 	
 	public Level2() {
 		platforms = new ArrayList<Block>();
-		//level3 = new Level3();
+		platforms.add(new Block(new Vector(0, 0, 0), new Vector(2, 0.5f, 2)));
+		platforms.add(getEndBlock());
 	}
 	
 	public List<Block> getBlocks() {
-		platforms.add(new Block(new Vector(-1, -2, -1), new Vector(8, -1, 8)));
-		platforms.add(new Block(new Vector(2, -1, 2), new Vector(4, -0.5f, 4)));
-		platforms.add(getEndBlock());
 		return platforms;
 	}
 
